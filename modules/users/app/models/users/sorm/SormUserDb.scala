183f9ea6e9b36e6e302961fecc09863880208b94
Honain Khan <honaink@gmail.com>
2014-02-05 23:13:46 -0600

    fixed reset password bug.  Code needs a refactoring but bug has been finally squashed.
    On a long enough timeline the survival rate for everyone drops to 0

--- modules/users/app/models/users/sorm/SormUserDb.scala
+++ modules/users/app/models/users/sorm/SormUserDb.scala
@@ -5,6 +5,7 @@ import org.joda.time.DateTime
 import securesocial.core.IdentityId
 import securesocial.core.providers.Token
 import models.users.UserDb
+import sorm.Persisted
 
 
 object SormUserDb extends UserDb {
@@ -23,22 +24,33 @@ object SormUserDb extends UserDb {
   }
 
   def save(identity: Identity): SocialUser = {
-    SormDb.query[SocialUser].whereEqual("email", identity.email).fetchOne().getOrElse(
-      SormDb.transaction {
-        SormDb.save(SocialUser(
-          SormDb.save(identity.identityId),
-          identity.firstName,
-          identity.lastName,
-          identity.fullName,
-          identity.email,
-          identity.avatarUrl,
-          SormDb.save(identity.authMethod),
-          identity.oAuth1Info.map(e => SormDb.save(e)),
-          identity.oAuth2Info.map(e => SormDb.save(e)),
-          identity.passwordInfo.map(e => SormDb.save(e))
-        ))
-      }
-    )
+    val socialUser: Option[SocialUser with Persisted] = SormDb.query[SocialUser].whereEqual("email", identity.email).fetchOne()
+    socialUser match {
+      case None => val newUser = SocialUser(
+        SormDb.save(identity.identityId),
+        identity.firstName,
+        identity.lastName,
+        identity.fullName,
+        identity.email,
+        identity.avatarUrl,
+        SormDb.save(identity.authMethod),
+        identity.oAuth1Info.map(e => SormDb.save(e)),
+        identity.oAuth2Info.map(e => SormDb.save(e)),
+        identity.passwordInfo.map(e => SormDb.save(e)))
+        SormDb.transaction {
+          SormDb.save(newUser)
+        }
+        newUser
+      case Some(existingUser) =>
+        val passInfo = Some(SormDb.save(identity.passwordInfo.get))
+        val updatedUser = existingUser.copy(identityId = identity.identityId, firstName = identity.firstName,
+        lastName = identity.lastName, fullName = identity.fullName, email = identity.email, avatarUrl = identity.avatarUrl, authMethod = identity.authMethod,
+        oAuth1Info = identity.oAuth1Info, oAuth2Info = identity.oAuth2Info, passwordInfo = passInfo)
+        SormDb.transaction {
+          SormDb.save(updatedUser)
+        }
+        updatedUser
+    }
   }
 
   def save(token: Token) = {
@@ -54,10 +66,12 @@ object SormUserDb extends UserDb {
   }
 
   def deleteExpiredTokens() {
-    SormDb.query[Token].whereSmallerOrEqual("expirationTime", DateTime.now()).fetch() foreach { SormDb.delete(_) }
+    SormDb.query[Token].whereSmallerOrEqual("expirationTime", DateTime.now()).fetch() foreach {
+      SormDb.delete(_)
+    }
   }
 
-  def findAll:List[SocialUser] = {
+  def findAll: List[SocialUser] = {
     SormDb.query[SocialUser].fetch().toList
   }
 
