3941d5c7671de40d82213440dd6fd723c621e922
Joseph Dessens <knockingspirit@users.noreply.github.com>
2014-07-23 10:50:32 -0500

    Reverted the version back to 1.0.0-SNAPSHOT

--- project/Common.scala
+++ project/Common.scala
@@ -4,7 +4,7 @@ import sbt._
 object Common {
   val settings: Seq[Setting[_]] = Seq(
     organization := "so.paws",
-    version := "1.1.0",
+    version := "1.0.0-SNAPSHOT",
     scalaVersion := "2.10.4"
   )
 }
